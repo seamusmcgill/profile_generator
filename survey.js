fb47b955fdb1251c23ab38110ad886eed510ae93
Seamus McGill <seamusmcgill@gmail.com>
2022-01-18 14:06:18 -0500

    added comments

--- survey.js
+++ survey.js
@@ -5,6 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Ask questions one after the other - make each rl.question's callback the next question until final output
 rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
   rl.question("What's an activity you like doing? ", (hobby) => {
     rl.question("What do you listen to while doing that? ", (music) => {
@@ -12,9 +13,11 @@ rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
         rl.question("What's your favourite thing to eat for that meal? ", (food) => {
           rl.question("Which sport is your absolute favourite? ", (sport) => {
             rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (superpower) => {
+              // Output string broken onto several lines for readability
               console.log(`${name} loves listening to ${music} while ${hobby},` +
               ` devouring ${food} for ${meal}, prefers ${sport} over any other sport,` +
               ` and is amazing at ${superpower}.`);
+              // Close the readling after the bio has been generated
               rl.close();
             });
           });
